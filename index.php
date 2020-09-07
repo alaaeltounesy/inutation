6cca96c3c79f1f4340d8287730aee08c3d53da42
Emad <elbembo@users.noreply.github.com>
2020-09-07 17:10:11 +0200

    g

--- index.php
+++ index.php
@@ -1,14 +1,13 @@
 <?php
-require __DIR__."/vendor/autoload.php";
+require 'vendor/autoload.php';
 $klein = new \Klein\Klein();
 
 $klein->respond('GET', "/", function ($request) {
 
-    require_once __DIR__ . "/home.php";
+    require_once  "home.php";
 });
-$klein->respond('GET', "/about", function ($request) {
-
-    require_once __DIR__ . "/about.php";
+$klein->respond('GET', '/Services', function () {
+    require 'Services.php';
 });
 
 
