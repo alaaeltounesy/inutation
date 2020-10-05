365a1530184439217a150cd0b55f30b2faec5b22
Emad <elbembo@users.noreply.github.com>
2020-10-05 17:44:09 +0200

    s

--- Contact.php
+++ Contact.php
@@ -150,9 +150,6 @@ include 'share.php';
         <div class="row social-contact col-xl-6 col-md-9 col-7 mx-auto justify-content-center p-0  mt-5">
             <div class="   col-md-6 col-xl-5  p-0 my-md-5 my-3">
 
-<<<<<<< Updated upstream
-                <a target="_black" class="italic-SemiBold black mt-5" href="https://www.facebook.com/ShadiaHamadeh/">
-=======
     <section class="sec2-c py-5 px-lg-0 px-2">
         <div class="container-fluid w-85 py-5 ">
             <div class="row " style="border:2px solid black;">
@@ -224,7 +221,6 @@ Are you tired of overthinking what food might cause you harm?
                         CONNECT WITH <span class="SemiBold"> SHADIA</span>
                     </h3>
                 </div>
->>>>>>> Stashed changes
 
                     <svg xmlns="http://www.w3.org/2000/svg" alt="facebook" id="facebook" width="25" height="25" class="mr-lg-4 mr-2" viewBox="0 0 17.722 32.779">
                         <path id="Path_1" data-name="Path 1" d="M39.132.007,34.881,0c-4.776,0-7.862,3.166-7.862,8.067v3.719H22.745a.668.668,0,0,0-.668.669v5.389a.668.668,0,0,0,.668.668h4.274v13.6a.668.668,0,0,0,.668.668h5.576a.668.668,0,0,0,.668-.668v-13.6h5a.668.668,0,0,0,.668-.668l0-5.389a.669.669,0,0,0-.669-.669h-5V8.633c0-1.515.361-2.285,2.335-2.285h2.863a.668.668,0,0,0,.668-.668v-5A.669.669,0,0,0,39.132.007Z" transform="translate(-22.077)" />
--- mail.php
+++ mail.php
@@ -4,7 +4,7 @@ $message ="";
 if($_SERVER['REQUEST_METHOD'] == 'POST' ){
     $message .= "Name : ".$_POST['name']."\r\n";
     $message .= "Email : ".$_POST['email']."\r\n";
-    $message .= "Subject : ".$_POST['message1']."\r\n";
+    $message .= "How did you find me?  ".$_POST['message1']."\r\n";
     $message .= "Message : ".$_POST['message2']."\r\n";;
     $message = wordwrap($message, 70, "\r\n");
     $headers = 'From: ' .$_POST['email']. "\r\n" .
